21ea2d4a5d2747382673dd431595ce8241a159c9
wangshaobo <wangshaobo08@gmail.com>
2018-12-07 15:28:14 +0800

    pep8 sublime

--- bubbleSort.py
+++ bubbleSort.py
@@ -4,16 +4,15 @@
 # time: 2018/11/29
 # soberwone@gmail.com
 
-import sys,os
 
 def bubbleSort(arr):
-	count = len(arr)
-	for j in range(count-1,0,-1):
-		for i in range(j):
-			if arr[i] > arr[i + 1]:
-				arr[i],arr[i + 1] = arr[i + 1],arr[i]
+    count = len(arr)
+    for j in range(count - 1, 0, -1):
+        for i in range(j):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
 
 if __name__ == "__main__":
-	arr = [1,3,2,55,33,4,3]
-	bubbleSort(arr)
-	print(arr)
+    arr = [1, 3, 2, 55, 33, 4, 3]
+    bubbleSort(arr)
+    print(arr)
